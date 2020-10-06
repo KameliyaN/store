0adbb43b9f9566bc299c94d287c66bb5155f71fd
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-10-06 12:46:04 +0300

    added model
    added model

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Animal(models.Model):
+    KIND_CHOICES = (
+        ('D', 'dog'),
+        ('C', 'cat')
+    )
+    name = models.CharField(max_length=20)
+    age = models.PositiveIntegerField()
+    breed = models.CharField(max_length=20)
+    kind = models.CharField(max_length=1, choices=KIND_CHOICES)
+    image_url = models.URLField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} and i am {self.age}'
